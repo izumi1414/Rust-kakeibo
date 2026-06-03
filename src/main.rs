53231573e2a68d163ea50228c2711a140e21eaac
izumi1414 <izumitku99@gmail.com>
2026-06-03 16:35:38 +0900

    feat: implement transaction input handling

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 use kakeibo::services;
 
+const FILE_PATH: &str = "store/json";
+
 fn main() {
     let mut service_type = String::new();
     println!("実行したい内容を入力してください（0:登録、1:集計）");
@@ -9,7 +11,7 @@ fn main() {
     services::validate::InputValidator::validate_service_type(service_type);
 
     if service_type == 0 {
-      println!("登録サービス");
+      services::register::run(FILE_PATH)
     } else {
       println!("集計サービス");
     }
